89e3a85145cc71afd2af00b3ec5f862e813e950e
modems <syslao@gmail.com>
2017-07-28 00:18:14 +0300

    refactoring, removed json format from route controller

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -17,32 +17,24 @@ class RoutesController < ApplicationController
 
   def create
     @route = Route.new(route_params)
-
-    respond_to do |format|
-      if @route.save
-        format.html { redirect_to @route, notice: 'route was successfully created.' }
-      else
-        format.html { render :new }
-      end
+    if @route.save
+      redirect_to @route, notice: 'route was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @route.update(route_params)
-        format.html { redirect_to @route, notice: 'route was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
+    if @route.update(route_params)
+      redirect_to @route, notice: 'route was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     @route.destroy
-    respond_to do |format|
-      format.html { redirect_to routes_url, notice: 'route was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to routes_url, notice: 'route was successfully destroyed.'
   end
 
   private

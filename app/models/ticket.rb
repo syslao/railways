708a599e73c0d79da44bcfebd07921d6d8294c8f
syslao <syslao@gmail.com>
2017-07-31 23:50:11 +0300

    add fix

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,6 @@ class Ticket < ActiveRecord::Base
   belongs_to :train
   belongs_to :departure_station, class_name: 'RailwayStation'
   belongs_to :destination_station, class_name: 'RailwayStation'
+
+  validates :number, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_many :tickets
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end

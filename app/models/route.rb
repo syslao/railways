c4adcdc29585dfa2fae88b0611fa049c1abac43a
modems <syslao@gmail.com>
2017-12-09 18:32:05 +0300

    add purchase support and some styling

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,6 +2,5 @@ class Route < ActiveRecord::Base
   has_many :railway_stations_routes
   has_many :trains
   has_many :railway_stations, through: :railway_stations_routes
-  
   validates :name, presence: true
 end
